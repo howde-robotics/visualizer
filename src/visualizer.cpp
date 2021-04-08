d410f4c63cff3a7e731d1aba032b682986aa5cd9
Dan Bronstein <danbronzy@gmail.com>
2021-04-08 10:46:58 -0400

    Map, pose and tfs added

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -2,11 +2,14 @@
 #include <std_msgs/Int32.h>
 #include <std_msgs/ColorRGBA.h>
 #include <visualization_msgs/Marker.h>
-#include "dragoon_messages/watchHeartbeat.h"
-#include "dragoon_messages/watchStatus.h"
 #include <jsk_rviz_plugins/OverlayText.h>
 #include <sstream>
 #include <vector>
+
+#include "dragoon_messages/watchHeartbeat.h"
+#include "dragoon_messages/watchStatus.h"
+// #include "dragoon_messages/ObjectInfo.h"
+#include "dragoon_messages/Objects.h"
 
 std::vector<char> status;
 
@@ -19,13 +22,21 @@ void healthSubCB(const dragoon_messages::watchStatusConstPtr& msg)
     
 }
 
+void objectPosesCB(const dragoon_messages::ObjectsConstPtr& msg) 
+{
+    ROS_INFO_STREAM("Human Detection callback");
+
+}
+
 int main(int argc, char** argv)
 {
 ros::init(argc, argv, "visualizer");
 ros::NodeHandle nh;
-ros::Rate loop_rate(1);
+ros::Rate loop_rate(10);
 
 ros::Subscriber health_sub = nh.subscribe("/health_status", 5, healthSubCB);
+
+ros::Subscriber human_det_sub = nh.subscribe("/ObjectPoses", 5, objectPosesCB);
 
 ros::Publisher overlay_pub = nh.advertise<jsk_rviz_plugins::OverlayText>("guiText", 10);
 jsk_rviz_plugins::OverlayText text;
@@ -42,23 +53,23 @@ text.font = "DejaVu Sans Mono";
 
 
 
-ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "dragoon_mesh", 0, true);
+ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "dragoon_mesh", 0);
 visualization_msgs::Marker marker;
-marker.header.frame_id = "base_link";
+marker.header.frame_id = "/base_link";
 marker.header.stamp = ros::Time();
 // marker.ns = "my_namespace";
 marker.id = 0;
 marker.type = visualization_msgs::Marker::MESH_RESOURCE;
-marker.pose.position.x = -0.5;
-marker.pose.position.y = 0.875;
-marker.pose.position.z = -.25;
+marker.pose.position.x = -0.32;
+marker.pose.position.y = 0.35;
+marker.pose.position.z = -0.15;
 marker.pose.orientation.x = 0.7071068;
 marker.pose.orientation.y = 0.0;
 marker.pose.orientation.z = 0.0;
 marker.pose.orientation.w = 0.7071068;
-marker.scale.x = 0.00254;
-marker.scale.y = 0.00254;
-marker.scale.z = 0.00254;
+marker.scale.x = 0.001;
+marker.scale.y = 0.001;
+marker.scale.z = 0.001;
 marker.color.a = 1.0; // Don't forget to set the alpha!
 marker.color.r = 1.0;
 marker.color.g = 0.0;
@@ -67,7 +78,7 @@ marker.mesh_resource = "package://visualizer/meshes/DRAGOON_SINGLE_bin.STL";
 vis_pub.publish( marker );
 int count = 0;
 while(ros::ok()) {
-    // vis_pub.publish( marker );
+    vis_pub.publish(marker);
     ++count;
     std::stringstream ss;
     ss << "Health Status" << std::endl;
